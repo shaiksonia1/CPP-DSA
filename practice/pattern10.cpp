cca3dbb77732a8b0bd34ee39046f835c093f6df2
Shaik Sonia <79378738+shaiksonia1@users.noreply.github.com>
2023-01-05 20:19:31 +0000

    commit

---
+++ practice/pattern10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+void printTriangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n - 1; i > 0; i--)
+
+    {
+
+        for (int j = 1; j <= i; j++)
+
+        {
+
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n = 5;
+    cout << "printTriangle" << endl;
+    printTriangle(n);
+}
